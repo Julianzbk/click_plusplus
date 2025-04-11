82e9b6a6f616d314a1645d93618b24cc68b7c851
Julian Zhang <julianbz@uci.edu>
2025-04-11 14:55:05 -0700

    Before removing uses of bfloat16_t

--- SGDClassifier.hpp
+++ SGDClassifier.hpp
@@ -1,3 +1,6 @@
+#pragma once
+
+#include <iostream>
 #include <format>
 #include <cmath>
 #include <numeric>
@@ -8,11 +11,6 @@
 #include <cassert>
 
 #include "vectors.hpp"
-
-#include <iostream>
-using std::cout, std::endl;
-
-using bf16 = std::bfloat16_t;
 
 #pragma region utility
 inline bool float_approx(double a, double b, double epsilon = 1e-9)
@@ -52,7 +50,7 @@ public:
     }
 };
 
-template <class dtype, size_t M>
+template <class dtype>
 double error(std::vector<dtype> const& pred,
              std::vector<dtype> const& Y)
 {
@@ -76,24 +74,30 @@ class SGDClassifier
     using Matrix = std::vector<std::array<dtype, M>>; // use if you'd like.
 public:
     std::array<dtype, M> theta_;
-    std::array<std::string, M> classes_;
     dtype bias_;
     float lr;
+    float lambda;
     uint32_t max_epochs;
+    bool early_stop;
     static constexpr uint32_t CALC_LOSS_EVERY = 1;
-    static constexpr uint32_t PRINT_LOSS_EVERY = 5; // should be a multiple of CALC_LOSS_EVERY,
+    static constexpr uint32_t PRINT_LOSS_EVERY = 1; // should be a multiple of CALC_LOSS_EVERY,
                                                     // set to 0 to disable print.
     static constexpr double CONVERGED_THRESH = 1e-4;
 
-    SGDClassifier(float lr = 0.01, uint32_t max_epochs = 100)
+    SGDClassifier(float lr = 0.01, float lambda = 0.01, uint32_t max_epochs = 100, bool early_stop = false)
         :theta_(std::array<dtype, M>()), bias_(dtype()),
-         lr(lr), max_epochs(max_epochs)
+         early_stop(early_stop), lr(lr), max_epochs(max_epochs)
     {
     }
 
     static inline dtype sigmoid(dtype z)
     {
         return 1 / (1 + std::exp(-z));
+    }
+
+    static double loss(dtype h, dtype y)
+    {
+        return -(y * log(h + 1e-15) + (1 - y) * log(1 - h + 1e-15));
     }
 
     static double loss(dtype h, std::vector<dtype> const& Y)
@@ -136,15 +140,17 @@ public:
         for (; epoch <= max_epochs; ++epoch)
         {
             size_t i = random_state.generate(); // Stoichastic = choose random datapoint from X.
-            //for (size_t i = 0; i < X.size(); ++i) // Use for loop instead to train on full dataset.
+            // std::cout << "i = " << i << "\n";
+            // for (size_t i = 0; i < X.size(); ++i) // Use for loop instead to train on full dataset.
             {
                 std::array<dtype, M> xi = X[i];
+                xi[0] = 0;
                 dtype yi = Y[i];
                 dtype z = dot(xi, theta_) + bias_;
                 dtype h = sigmoid(z);
                 dtype error = h - yi;
                 std::array<dtype, M> grad = error * xi; // scalar mult with an array here.
-                theta_ -= lr * grad; // another scalar mult.
+                theta_ -= lr * (grad + lambda * theta_); // another scalar mult.
                 bias_ -= lr * error;
             }
             
@@ -158,20 +164,38 @@ public:
                 loss = this->loss(Z, Y);
 
                 if (PRINT_LOSS_EVERY > 0 && epoch % PRINT_LOSS_EVERY == 0)
-                    std::cout << std::format("Epoch {}: Loss = {:.5f}\n", epoch, loss);
+                    std::cout << std::format("Epoch {}: Loss = {:.10f}\n", epoch, loss);
 
-                if (float_approx(last_losses[0], loss, CONVERGED_THRESH) &&
+                if (early_stop &&
+                    float_approx(last_losses[0], loss, CONVERGED_THRESH) &&
                     float_approx(last_losses[1], loss, CONVERGED_THRESH))
                 {
-                    std::cout << "Loss has converged!" << endl;
+                    std::cout << "Loss has converged!" << std::endl;
                     ++epoch;
                     break;
                 }
                 last_losses[1] = last_losses[0];
                 last_losses[0] = loss;
             }
+        }   
+        std::cout << std::format("Training has ended after {} Epochs with Loss = {:.10f}.", epoch - 1, loss) << std::endl;
+    }
+
+    void partial_fit(std::array<dtype, M> const& x, dtype const& y)
+    {
+        dtype z = dot(x, theta_) + bias_;
+        dtype h = sigmoid(z);
+        dtype error = h - y;
+        std::array<dtype, M> grad = error * x; // scalar mult with an array here.
+        theta_ -= lr * grad; // another scalar mult.
+        bias_ -= lr * error;
+            
+        if (PRINT_LOSS_EVERY > 0)
+        {
+            dtype z = dot(theta_, x);
+            z = sigmoid(z + bias_);
+            std::cout << std::format("Partial-fit: Loss = {:.5f}\n", loss(z, y));
         }
-        std::cout << std::format("Training has ended after {} Epochs with Loss = {:.5f}.", epoch - 1, loss) << endl;
     }
 
     std::vector<dtype> predict_proba(std::vector<std::array<dtype, M>> const& X)
@@ -193,9 +217,12 @@ public:
     }
 };
 
-int test_example()
+void test_example()
 {
-    auto model = SGDClassifier<bf16, 2>(0.1, 1000);
+    /*
+    using bf16 = std::bfloat16_t;
+    using std::cout, std::endl;
+    auto model = SGDClassifier<bf16, 2>(0.1, 0.01, 1000);
     std::vector<std::array<bf16, 2>> X_tr(4);
     X_tr[0] = std::array<bf16, 2>({1.0, 2.0});
     X_tr[1] = std::array<bf16, 2>({0.25, 0.2});
@@ -205,4 +232,5 @@ int test_example()
     model.fit(X_tr, y_tr);
     cout << model.theta_ << ", " << model.bias_ << endl;
     cout << model.predict({{2, 1}, {0, -1}, {0, 3}}) << endl;
+    */
 }
