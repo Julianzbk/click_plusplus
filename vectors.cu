0c88ec7e627f05300027da1d83bfaba1a10760c1
Julian Zhang <julianbz@uci.edu>
2025-04-14 15:31:10 -0700

    Information erasure with using

--- vectors.cu
+++ vectors.cu
@@ -3,9 +3,11 @@
 #include <cstdlib>
 #include <cstdio>
 
+constexpr size_t threads_per_block = 256;
+
 template <typename dtype = float>
 __global__
-void dot_product_vector(const dtype* A, const dtype* B, dtype* partial, size_t N)
+void dot_vector(const dtype* A, const dtype* B, dtype* partial, size_t N)
 {
     extern __shared__ dtype cache[];
 
@@ -31,13 +33,13 @@ void dot_product_vector(const dtype* A, const dtype* B, dtype* partial, size_t N
     }
 }
 
-extern "C" float dot_product_vector_float(const float* h_A, const float* h_B, size_t N)
+
+extern "C" float dot_vector_float(const float* h_A, const float* h_B, size_t N)
 {
     /*
         Copies host vector to cudaMalloc'd vector, and performs dot product,
         accumulating partial sums and returning their sum.
     */
-    constexpr size_t threads_per_block = 256;
     const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
     const size_t size = N * sizeof(float);
 
@@ -48,7 +50,7 @@ extern "C" float dot_product_vector_float(const float* h_A, const float* h_B, si
     cudaMemcpy(A, h_A, size, cudaMemcpyHostToDevice);
     cudaMemcpy(B, h_B, size, cudaMemcpyHostToDevice);
 
-    dot_product_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+    dot_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
         (A, B, partial, N);
     
     float* h_partial = new float[blocks_per_grid];
@@ -65,4 +67,168 @@ extern "C" float dot_product_vector_float(const float* h_A, const float* h_B, si
     delete[] h_partial;
 
     return prod;
+}
+
+extern "C" float device_dot_vector_float(const float* d_A, const float* d_B, size_t N)
+{
+    /*
+        Performs dot product directly on device memory.
+    */
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* partial;
+    cudaMalloc(&partial, blocks_per_grid * sizeof(float));
+
+    dot_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (d_A, d_B, partial, N);
+    
+    float* h_partial = new float[blocks_per_grid];
+    cudaMemcpy(h_partial, partial, blocks_per_grid * sizeof(float), cudaMemcpyDeviceToHost);
+
+    float prod = 0;
+    for (size_t i = 0; i < blocks_per_grid; ++i)
+    {
+        prod += h_partial[i];
+    }
+
+    cudaFree(partial);
+    delete[] h_partial;
+    return prod;
+}
+
+
+template <class dtype>
+__global__
+void vector_add_vector(const dtype* V, const dtype* U, dtype* dest, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest[i] = (i < N) ? V[i] + U[i] : dtype(); // operation step
+}
+
+extern "C" float* vector_add_vector_float(const float* V, const float* U, size_t N)
+{// Transfers ownership of a live pointer!
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* dest;
+    cudaMalloc(&dest, N * sizeof(float));
+
+    vector_add_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, U, dest, N);
+    
+    return dest;
+}
+
+template <class dtype>
+__global__
+void vector_addassign_vector(dtype* V, const dtype* U, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    V[i] += (i < N) ? U[i] : dtype();
+}
+
+extern "C" void vector_addassign_vector_float(float* V, const float* U, size_t N)
+{
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    vector_addassign_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, U, N);
+}
+
+template <class dtype>
+__global__
+void vector_sub_scalar(const dtype* V, dtype A, dtype* dest, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest[i] += (i < N) ? V[i] - A : dtype();
+}
+
+extern "C" float* vector_sub_scalar_float(const float* V, float A, size_t N)
+{
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* dest;
+    cudaMalloc(&dest, N * sizeof(float));
+
+    vector_sub_scalar<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, A, dest, N);
+    return dest;
+}
+
+template <class dtype>
+__global__
+void vector_sub_vector(const dtype* V, const dtype* U, dtype* dest, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest[i] = (i < N) ? V[i] - U[i] : dtype(); // operation step
+}
+
+extern "C" float* vector_sub_vector_float(const float* V, const float* U, size_t N)
+{// Transfers ownership of a live pointer!
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* dest;
+    cudaMalloc(&dest, N * sizeof(float));
+
+    vector_sub_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, U, dest, N);
+    
+    return dest;
+}
+
+template <class dtype>
+__global__
+void vector_subassign_vector(dtype* V, const dtype* U, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    V[i] -= (i < N) ? U[i] : dtype();
+}
+
+extern "C" void vector_subassign_vector_float(float* V, const float* U, size_t N)
+{
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    vector_subassign_vector<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, U, N);
+}
+
+template <class dtype>
+__global__
+void vector_mul_scalar(const dtype* V, dtype A, dtype* dest, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest[i] += (i < N) ? V[i] * A : dtype();
+}
+
+extern "C" float* vector_mul_scalar_float(const float* V, float A, size_t N)
+{// Transfers ownership of a live pointer!
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* dest;
+    cudaMalloc(&dest, N * sizeof(float));
+
+    vector_mul_scalar<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, A, dest, N);
+    
+    return dest;
+}
+
+template <class dtype>
+__global__
+void vector_div_scalar(const dtype* V, dtype A, dtype* dest, size_t N)
+{
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    dest[i] += (i < N) ? V[i] / A : dtype();
+}
+
+extern "C" float* vector_div_scalar_float(const float* V, float A, size_t N)
+{// Transfers ownership of a live pointer!
+    const size_t blocks_per_grid = (N + threads_per_block - 1) / threads_per_block;
+
+    float* dest;
+    cudaMalloc(&dest, N * sizeof(float));
+
+    vector_div_scalar<float> <<<blocks_per_grid, threads_per_block, threads_per_block * sizeof(float)>>>
+        (V, A, dest, N);
+    
+    return dest;
 }
--- vectors_device.hpp
+++ vectors_device.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <concepts>
+#include <cassert>
+#include <initializer_list>
 
 #pragma region containers
 #include <array>
@@ -78,7 +80,7 @@ nv_bfloat16 operator / (nv_bfloat16 lhs, itype rhs)
 #pragma endregion bf16_operators
 
 
-extern "C" float dot_product_vector_float(const float* A, const float* B, size_t N);
+extern "C" float dot_vector_float(const float* A, const float* B, size_t N);
 
 
 // Overloading operators for only scalar operations, or simple operations with the same type.
@@ -128,12 +130,12 @@ std::vector<dtype> operator - (std::vector<dtype> const& V, dtype A)
 template <typename dtype, size_t M>
 std::array<dtype, M> operator - (std::array<dtype, M> const& V, std::array<dtype, M> const& U)
 {
-    std::array<dtype, M> sum;
+    std::array<dtype, M> diff;
     for (size_t i = 0; i < M; ++i)
     {
-        sum[i] = V[i] - U[i];
+        diff[i] = V[i] - U[i];
     }
-    return sum;
+    return diff;
 }
 
 template <typename dtype, size_t M>
@@ -175,20 +177,29 @@ template <typename dtype>
 inline dtype dot(std::vector<dtype> const& V, std::vector<dtype> const& U)
 {
     assert(V.size() == U.size());
-    return dot_product_vector_float(V.data(), U.data(), V.size());
+    if constexpr (std::is_same_v<dtype, float>)
+        return dot_vector_float(V.data(), U.data(), V.size());
+    else
+        std::cout << "dot: dtype not implemented!" << std::endl;
 }
 
 template <typename dtype, size_t M>
 inline dtype dot(std::vector<dtype> const& V, std::array<dtype, M> const& U)
 {
     assert(V.size() == U.size());
-    return dot_product_vector_float(V.data(), U.data(), V.size());
+    if constexpr (std::is_same_v<dtype, float>)
+        return dot_vector_float(V.data(), U.data(), M);
+    else
+        std::cout << "dot: dtype not implemented!" << std::endl;
 }
 
 template <typename dtype, size_t M>
 inline dtype dot(std::array<dtype, M> const& V, std::array<dtype, M> const& U)
 {
-    return dot_product_vector_float(V.data(), U.data(), M);
+    if constexpr (std::is_same_v<dtype, float>)
+        return dot_vector_float(V.data(), U.data(), M);
+    else
+        std::cout << "dot: dtype not implemented!" << std::endl;
 }
 
 template <typename dtype, size_t M>
@@ -198,6 +209,302 @@ std::vector<dtype> dot(std::array<dtype, M> const& T,
 {
     size_t N = X.size();
     std::vector<dtype> Y(N);
+    for (size_t i = 0; i < N; ++i)
+    {
+        Y[i] = dot(X[i], T) + bias;
+    }
+    return Y;
+}
+
+
+
+template <class dtype>
+class DeviceVector
+{
+public:
+    size_t size;
+    dtype* buf; // Device side
+
+    DeviceVector()
+        :size(0), buf(nullptr)
+    {
+    }
+
+    DeviceVector(std::initializer_list<dtype> const& V)
+        :size(V.size())
+    {
+        cudaMalloc(&buf, V.size() * sizeof(dtype));
+        cudaMemcpy(buf, std::data(V), V.size() * sizeof(dtype), cudaMemcpyHostToDevice);
+    }
+
+    DeviceVector(std::vector<dtype> const& V)
+        :size(V.size())
+    {
+        cudaMalloc(&buf, V.size() * sizeof(dtype));
+        cudaMemcpy(buf, V.data(), V.size() * sizeof(dtype), cudaMemcpyHostToDevice);
+    }
+
+    ~DeviceVector()
+    {
+        cudaFree(buf);
+    }
+
+    dtype operator [] (size_t idx) const
+    {// BAD
+        std::cout << "BAD!" << std::endl;
+        dtype host;
+        cudaMemcpy(&host, &buf[idx], sizeof(dtype), cudaMemcpyDeviceToHost);
+        return host;
+    }
+};
+
+template <typename T>
+std::ostream& operator << (std::ostream& out, DeviceVector<T> V)
+{
+    T* host = new T[V.size];
+    cudaMemcpy(host, V.buf, V.size * sizeof(T), cudaMemcpyDeviceToHost);
+
+    out << '[';
+    size_t i = 0;
+    size_t size = V.size();
+    for (; i < size - 1; ++i)
+    {
+        out << host[i] << ", ";
+    }
+    out << host[size - 1];
+    out << ']';
+    delete[] host;
+    return out;
+}
+
+
+template <class dtype, size_t M>
+class DeviceArray
+{
+public:
+    dtype* buf; // Device side
+
+    DeviceArray()
+        :buf(nullptr)
+    {
+    }
+
+    DeviceArray(std::initializer_list<dtype> const& A)
+    {
+        assert(A.size() == M);
+        cudaMalloc(&buf, M * sizeof(dtype));
+        cudaMemcpy(buf, std::data(A), M * sizeof(dtype), cudaMemcpyHostToDevice);
+    }
+
+    DeviceArray(std::array<dtype, M> const& A)
+    {
+        cudaMalloc(&buf, M * sizeof(dtype));
+        cudaMemcpy(buf, A.data(), M * sizeof(dtype), cudaMemcpyHostToDevice);
+    }
+
+    ~DeviceArray()
+    {
+        cudaFree(buf);
+    }
+
+    constexpr size_t size() const
+    {
+        return M;
+    }
+
+    dtype operator [] (size_t idx) const
+    {// BAD
+        dtype host;
+        cudaMemcpy(&host, &buf[idx], sizeof(dtype), cudaMemcpyDeviceToHost);
+        return host;
+    }
+};
+
+template <typename T, size_t N>
+std::ostream& operator << (std::ostream& out, DeviceArray<T, N> const& A)
+{
+    static_assert(N > 0);
+
+    T host[N];
+    cudaMemcpy(host, A.buf, N * sizeof(T), cudaMemcpyDeviceToHost);
+    
+    out << '[';
+    size_t i = 0;
+    for (; i < N - 1; ++i)
+    {
+        out << host[i] << ", ";
+    }
+    out << host[N - 1];
+    out << ']';
+    return out;
+}
+
+
+template <class dtype, size_t M>
+class DeviceMatrix
+{
+public:
+    size_t size;
+    dtype* buf; // Device side
+
+    DeviceMatrix(std::vector<std::array<dtype, M>> const& V)
+        :size(V.size())
+    {
+        cudaMalloc(&buf, V.size() * M * sizeof(dtype));
+        dtype* dest = buf;
+        for (std::array<dtype, M> x: V)
+        {
+            cudaMemcpy(dest, x.data(), x.size() * sizeof(dtype), cudaMemcpyHostToDevice);
+            dest += M;
+        }
+    }
+
+    size_t N() const
+    {
+        return size;
+    }
+
+    DeviceArray<dtype, M> operator [] (size_t idx) const
+    {
+        DeviceArray<dtype, M> x;
+        x.buf = buf + idx * M;
+        return x;
+    }
+};
+
+template <typename T, size_t N>
+std::ostream& operator << (std::ostream& out, DeviceMatrix<T, N> const& M)
+{
+    static_assert(N > 0);
+
+    out << "Matrix(" << N << ", " << M.size << ")";
+    return out;
+}
+
+extern "C" float* vector_add_vector_float(const float* V, const float* U, size_t N);
+
+template <typename dtype, size_t M>
+DeviceArray<dtype, M> operator + (DeviceArray<dtype, M> const& V, DeviceArray<dtype, M> const& U)
+{
+    DeviceArray<dtype, M> sum;
+    if constexpr (std::is_same_v<dtype, float>)
+        sum.buf = vector_add_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "operator + : dtype not implemented!" << std::endl;
+    return sum;
+}
+
+extern "C" void vector_addassign_vector_float(float* V, const float* U, size_t N);
+
+template <typename dtype, size_t M>
+DeviceArray<dtype, M>& operator += (DeviceArray<dtype, M>& V, DeviceArray<dtype, M> const& U)
+{
+    if constexpr (std::is_same_v<dtype, float>)
+        vector_addassign_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "operator + : dtype not implemented!" << std::endl;
+    return V;
+}
+
+extern "C" float* vector_sub_scalar_float(const float* V, float A, size_t N);
+
+template <typename dtype>
+DeviceVector<dtype> operator - (DeviceVector<dtype> const& V, dtype A)
+{
+    DeviceVector<dtype> diff;
+    if constexpr (std::is_same_v<dtype, float>)
+        diff.buf = vector_sub_scalar_float(V.buf, A, V.size);
+    else
+        std::cout << "operator - : dtype not implemented!" << std::endl;
+    return diff;
+}
+
+extern "C" float* vector_sub_vector_float(const float* V, const float* U, size_t N);
+
+template <typename dtype, size_t M>
+DeviceArray<dtype, M> operator - (DeviceArray<dtype, M> const& V, DeviceArray<dtype, M> const& U)
+{
+    DeviceArray<dtype, M> diff;
+    if constexpr (std::is_same_v<dtype, float>)
+        diff.buf = vector_sub_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "operator - : dtype not implemented!" << std::endl;
+    return diff;
+}
+
+extern "C" void vector_subassign_vector_float(float* V, const float* U, size_t N);
+
+template <typename dtype, size_t M>
+DeviceArray<dtype, M>& operator -= (DeviceArray<dtype, M>& V, DeviceArray<dtype, M> const& U)
+{
+    if constexpr (std::is_same_v<dtype, float>)
+        vector_subassign_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "operator -= : dtype not implemented!" << std::endl;
+    return V;
+}
+
+extern "C" float* vector_mul_scalar_float(const float* V, float A, size_t N);
+
+template <typename itype, typename dtype, size_t M>
+DeviceArray<dtype, M> operator * (itype A, DeviceArray<dtype, M> const& V)
+{
+    static_assert(!std::is_same_v<itype, DeviceArray<dtype, M>>);
+    DeviceArray<dtype, M> U;
+    if constexpr (std::is_same_v<dtype, float>)
+        U.buf = vector_mul_scalar_float(V.buf, A, M);
+    else
+        std::cout << "operator * : dtype not implemented!" << std::endl;
+    return U;
+}
+
+extern "C" float* vector_div_scalar_float(const float* V, float A, size_t N);
+
+template <typename itype, typename dtype, size_t M>
+DeviceArray<dtype, M> operator / (DeviceArray<dtype, M> const& V, itype A)
+{
+    static_assert(!std::is_same_v<itype, DeviceArray<dtype, M>>);
+    DeviceArray<dtype, M> U;
+    if constexpr (std::is_same_v<dtype, float>)
+        U.buf = vector_div_scalar_float(V.buf, A, M);
+    else
+        std::cout << "operator / : dtype not implemented!" << std::endl;
+    return U;
+}
+
+template <typename dtype>
+inline dtype dot(DeviceVector<dtype> const& V, DeviceVector<dtype> const& U)
+{
+    assert(V.size == U.size);
+    return dot_vector_float(V.buf, U.buf, V.size);
+}
+
+template <typename dtype, size_t M>
+inline dtype dot(DeviceVector<dtype> const& V, DeviceArray<dtype, M> const& U)
+{
+    assert(V.size == M);
+    if constexpr (std::is_same_v<dtype, float>)
+        return dot_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "dot: dtype not implemented!" << std::endl;
+}
+
+template <typename dtype, size_t M>
+inline dtype dot(DeviceArray<dtype, M> const& V, DeviceArray<dtype, M> const& U)
+{
+    if constexpr (std::is_same_v<dtype, float>)
+        return dot_vector_float(V.buf, U.buf, M);
+    else
+        std::cout << "dot: dtype not implemented!" << std::endl;
+}
+
+template <typename dtype, size_t M>
+DeviceVector<dtype> dot(DeviceArray<dtype, M> const& T,
+                        DeviceVector<DeviceArray<dtype, M>> const& X,
+                        dtype bias = dtype())
+{
+    size_t N = X.size();
+    DeviceVector<dtype> Y(N);
     for (size_t i = 0; i < N; ++i)
     {
         Y[i] = dot(X[i], T) + bias;
